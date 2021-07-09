3339c0decd6183baf71b5cbdbce7ba7eb317612a
Isaias Souza <ij.souza1988@hotmail.com>
2021-07-09 00:35:08 -0300

    sync

---
+++ gateway/src/test/java/com/is4ii4s/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is4ii4s.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
